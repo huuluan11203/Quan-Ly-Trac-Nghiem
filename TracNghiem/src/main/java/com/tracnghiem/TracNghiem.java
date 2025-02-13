8c0277b01387556b45159063063e983f7443b1e1
huuluan11203 <huuluan11203@gmail.com>
2025-02-13 16:18:12 +0700

    first commit

---
+++ TracNghiem/src/main/java/com/tracnghiem/TracNghiem.java
@@ -0,0 +1,13 @@
+
+package com.tracnghiem;
+
+/**
+ *
+ * @author huulu
+ */
+public class TracNghiem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }   
+}
