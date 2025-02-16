4b598e6e5123fe0e61f711276b756aa888a989b3
huuluan11203 <huuluan11203@gmail.com>
2025-02-16 18:20:43 +0700

    MIT License

---
+++ TracNghiem/src/main/java/com/tracnghiem/main.java
@@ -0,0 +1,19 @@
+package com.tracnghiem;
+
+
+import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import javax.swing.SwingUtilities;
+import com.tracnghiem.view.loginView;
+
+public class main {
+    public static void main(String[] args) {
+        
+        FlatLaf.registerCustomDefaultsSource("themes");       
+        FlatLightLaf.setup();
+           
+        SwingUtilities.invokeLater(() -> {
+            new com.tracnghiem.view.mainView().setVisible(true);   // Khởi động giao diện chính
+        });
+    }
+}
